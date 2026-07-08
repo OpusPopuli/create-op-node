da577b2e83441cea4a629f4d26eae8463e36891f
Rodney Gagnon <43381472+rodneygagnon@users.noreply.github.com>
2026-07-08 15:38:17 -0700

    chore(main): release 0.11.2

--- src/cli.ts
+++ src/cli.ts
@@ -14,7 +14,7 @@ import { withDefaultSubcommand } from './lib/cli-args.js';
 // in package.json (via the manifest file) and updates literals on lines
 // like the one below.
 // x-release-please-start-version
-const VERSION = '0.11.1';
+const VERSION = '0.11.2';
 // x-release-please-end
 
 const program = new Command();
